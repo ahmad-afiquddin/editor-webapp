0864952465aa6ee4e07c0e34137f3b340a377508
Ahmad Afiquddin Bin Ahmad <ahmad_afiquddin@hotmail.com>
2019-04-11 22:48:59 +0800

    Update settings.py
    Removed secret key

--- editimages/settings.py
+++ editimages/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v%y7mhatk(*29k3%ta$22k^q_3bajrwa2a5)xlchwz*le*p6y1'
+SECRET_KEY = SECRET FOR A REASON
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
